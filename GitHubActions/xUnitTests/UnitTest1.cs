81fc12f121ee094d55934a8f8f16d22e6cfd086f
Sergio Fontes <sergio@computencial.com>
2021-07-17 11:56:08 +0100

    Nova versão

--- GitHubActions/xUnitTests/UnitTest1.cs
+++ GitHubActions/xUnitTests/UnitTest1.cs
@@ -10,5 +10,11 @@ namespace xUnitTests
         {
             Assert.Equal(1, 1);
         }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal(1, 1);
+        }
     }
 }
